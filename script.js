5c87ede56f739eb64fedd001ddf3e3a703e7b067
Jada Hooper <jhooper@st.noorderpoort.nl>
2025-06-23 20:50:37 +0200

    basic layout

---
+++ script.js
@@ -0,0 +1,68 @@
+import { gsap } from "gsap";
+
+const variants = [
+  {
+    logo: "Header_neutraal.png",
+    img: "Neutraal.png",
+    bottom_circle: "#1C2732",
+    text: {
+      features: [
+        { icon: "+", color: "#5BA9E6", text: "Ondersteunt sterke botten" },
+        { icon: "+", color: "#5BA9E6", text: "Natuurlijk glanzend haar" },
+        { icon: "+", color: "#5BA9E6", text: "Gemaakt in Nederland" },
+      ],
+      cta: {
+        text: "Probeer nu",
+        color: "#5BA9E6",
+      },
+    },
+  },
+  {
+    logo: "Header_neutraal.png",
+    img: "Neutraal.png",
+    bottom_circle: "#1C2732",
+    text: {
+      features: [
+        { icon: "+", color: "#5BA9E6", text: "Ondersteunt sterke botten" },
+        { icon: "+", color: "#5BA9E6", text: "Natuurlijk glanzend haar" },
+        { icon: "+", color: "#5BA9E6", text: "Gemaakt in Nederland" },
+      ],
+      cta: {
+        text: "Probeer nu",
+        color: "#5BA9E6",
+      },
+    },
+  },
+  {
+    logo: "Header_vanille_kokos.png",
+    img: "Vanille_kokos.png",
+    bottom_circle: "#1C2732",
+    text: {
+      features: [
+        { icon: "+", color: "#5BA9E6", text: "Ondersteunt sterke botten" },
+        { icon: "+", color: "#5BA9E6", text: "Natuurlijk glanzend haar" },
+        { icon: "+", color: "#5BA9E6", text: "Gemaakt in Nederland" },
+      ],
+      cta: {
+        text: "Probeer nu",
+        color: "#5BA9E6",
+      },
+    },
+  },
+  {
+    logo: "Header_cacao.png",
+    img: "Cacao.png",
+    bottom_circle: "#4A3526",
+    text: {
+      features: [
+        { icon: "+", color: "#C07B4A", text: "Ondersteunt sterke botten" },
+        { icon: "+", color: "#C07B4A", text: "Natuurlijk glanzend haar" },
+        { icon: "+", color: "#C07B4A", text: "Gemaakt in Nederland" },
+      ],
+      cta: {
+        text: "Probeer nu",
+        color: "#C07B4A",
+      },
+    },
+  },
+];
